ef212af9b5f5024d6352a44422eb996621fad39e
Onur Karal <santobixua@gmail.com>
2021-07-26 01:00:56 +0300

    (testing mc.movies Doc) adding rating

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -19,24 +19,28 @@ namespace dotnetcore.Models
                 // Look for any movies.
                 if (context.Movie.Any())
                 {
-                    return; // DB has been seeded
+                    return;   // DB has been seeded
                 }
 
+                #region snippet1
                 context.Movie.AddRange(
                     new Movie
                     {
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M
+                        Price = 7.99M,
+                        Rating = "R"
                     },
+                    #endregion
 
                     new Movie
                     {
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -44,7 +48,8 @@ namespace dotnetcore.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -52,7 +57,8 @@ namespace dotnetcore.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating = "NA"
                     }
                 );
                 context.SaveChanges();
